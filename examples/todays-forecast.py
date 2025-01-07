f3d6cce5bbe1f4c7ee7c0b60e8823183bd0e0076
tux <041337@proton.me>
2025-01-07 19:05:15 -0300

    Update to use ONNX

--- examples/todays-forecast.py
+++ examples/todays-forecast.py
@@ -3,19 +3,19 @@
 """
 
 import numpy as np
-import pickle as pk
 import pandas as pd
 import yfinance as yf
+import onnxruntime as ort
 
 
 # EMA (Exponential Moving Average)
-def EMA(span=10):
+def EMA(span=14):
     return df["Close"].ewm(span, adjust=False).mean()
 
 
 # MACD (Moving Average Convergence Divergence)
 def MACD():
-    return EMA(12) - EMA(26)
+    return EMA(6) - EMA(18)
 
 
 # RSI (Relative Strength Index)
@@ -33,13 +33,14 @@ def RSI(window=14):
 def LogReturn():
     return np.log(df["Close"] / df["Close"].shift(1))
 
+
 # Volatility
-def Volatility(window=10):
+def Volatility(window=7):
     return df["Close"].rolling(window).std()
 
 
 # Bollinger Bands
-def BollingerBands(window=20):
+def BollingerBands(window=14):
     rolling_mean = df["Close"].rolling(window=window).mean()
     rolling_std = df["Close"].rolling(window=window).std()
     upper_band = rolling_mean + (rolling_std * 2)
@@ -47,9 +48,14 @@ def BollingerBands(window=20):
     return upper_band, lower_band
 
 
-# Load model
-with open("dist/abyss.pkl", "rb") as file:
-    model = pk.load(file)
+# Momentum
+def Momentum(window=10):
+    return df["Close"] - df["Close"].shift(window)
+
+
+# Load ONNX model using onnxruntime
+onnx_model_path = "dist/abyss.onnx"  # Substitua com o caminho do seu modelo ONNX
+session = ort.InferenceSession(onnx_model_path)
 
 # Get data for the entire month
 df = yf.download("BTC-USD", period="1mo", progress=False)
@@ -57,18 +63,20 @@ df = yf.download("BTC-USD", period="1mo", progress=False)
 # Generate the indicators
 df["EMA"] = EMA()
 df["MACD"] = MACD()
+df["Momentum"] = Momentum()
 df["RSI"] = RSI()
 df["LogReturn"] = LogReturn()
 df["Volatility"] = Volatility()
 df["Upper Band"], df["Lower Band"] = BollingerBands()
 
-# Define independent (X) and dependent (y) variables
+# Define independent (X) variables for prediction
 X = df[
     [
         "Open",
         "High",
         "Low",
         "Close",
+        "Momentum",
         "Volume",
         "EMA",
         "MACD",
@@ -79,16 +87,21 @@ X = df[
         "Lower Band",
     ]
 ]
-y = df["Close"]
 
-# Forecast
-forecast = model.predict(X)
+# Prepare the input data in the right format (NumPy array)
+X_input = X.values.astype(np.float32)  # Convert to float32 for compatibility with ONNX
+
+# Get the model input name (usually "input" or something similar)
+input_name = session.get_inputs()[0].name
+
+# Perform the inference (prediction)
+forecast = session.run(None, {input_name: X_input})[0]
 
 # Show Forecast
 forecast_df = pd.DataFrame(
     {
         "Date": df.index,
-        "Predicted Close": forecast,
+        "Predicted Close": forecast.flatten(),  # Flatten to match the shape
         "Real Close": df["Close"].values.ravel(),
     }
 )
