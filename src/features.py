62c64d12293056decb506bb1b0f940c3877a7794
tux <041337@proton.me>
2025-01-07 19:05:44 -0300

    Change windows and introduce Momentum

--- src/features.py
+++ src/features.py
@@ -3,13 +3,13 @@ from data import df
 
 
 # EMA (Exponential Moving Average)
-def EMA(span=10):
+def EMA(span=14):
     return df["Close"].ewm(span, adjust=False).mean()
 
 
 # MACD (Moving Average Convergence Divergence)
 def MACD():
-    return EMA(12) - EMA(26)
+    return EMA(6) - EMA(18)
 
 
 # RSI (Relative Strength Index)
@@ -27,15 +27,21 @@ def RSI(window=14):
 def LogReturn():
     return np.log(df["Close"] / df["Close"].shift(1))
 
+
 # Volatility
-def Volatility(window=10):
+def Volatility(window=7):
     return df["Close"].rolling(window).std()
 
 
 # Bollinger Bands
-def BollingerBands(window=20):
+def BollingerBands(window=14):
     rolling_mean = df["Close"].rolling(window=window).mean()
     rolling_std = df["Close"].rolling(window=window).std()
     upper_band = rolling_mean + (rolling_std * 2)
     lower_band = rolling_mean - (rolling_std * 2)
     return upper_band, lower_band
+
+
+# Momentum
+def Momentum(window=10):
+    return df["Close"] - df["Close"].shift(window)
