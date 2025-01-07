3596c3c06c4f1a22109e8c8d004cfdf2ac19f9a2
tux <041337@proton.me>
2025-01-07 00:08:44 -0300

    Example of daily forecast using monthly data.

---
+++ examples/todays-forecast.py
@@ -0,0 +1,80 @@
+"""
+    Example of daily forecast using monthly data.
+"""
+
+import pickle as pk
+import pandas as pd
+import yfinance as yf
+
+# EMA (Exponential Moving Average)
+def EMA(span=10):
+    return df["Close"].ewm(span, adjust=False).mean()
+
+
+# MACD (Moving Average Convergence Divergence)
+def MACD():
+    return EMA(12) - EMA(26)
+
+
+# RSI (Relative Strength Index)
+def RSI(window=14):
+    delta = df["Close"].diff(1)
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=window).mean()
+    avg_loss = loss.rolling(window=window).mean()
+    rs = avg_gain / avg_loss
+    return 100 - (100 / (1 + rs))
+
+
+# Bollinger Bands
+def BollingerBands(window=20):
+    rolling_mean = df["Close"].rolling(window=window).mean()
+    rolling_std = df["Close"].rolling(window=window).std()
+    upper_band = rolling_mean + (rolling_std * 2)
+    lower_band = rolling_mean - (rolling_std * 2)
+    return upper_band, lower_band
+
+
+# Load model
+with open("dist/abyss.pkl", "rb") as file:
+    model = pk.load(file)
+
+# Get data for the entire month
+df = yf.download("BTC-USD", period="1mo", progress=False)
+
+# Generate the indicators
+df["EMA"] = EMA()
+df["MACD"] = MACD()
+df["RSI"] = RSI()
+df["Upper Band"], df["Lower Band"] = BollingerBands()
+
+# Define independent (X) and dependent (y) variables
+X = df[
+    [
+        "Open",
+        "High",
+        "Low",
+        "Close",
+        "Volume",
+        "EMA",
+        "MACD",
+        "RSI",
+        "Upper Band",
+        "Lower Band",
+    ]
+]
+y = df["Close"]
+
+# Forecast
+forecast = model.predict(X)
+
+# Show Forecast
+forecast_df = pd.DataFrame(
+    {
+        "Date": df.index,
+        "Predicted Close": forecast,
+        "Real Close": df["Close"].values.ravel(),
+    }
+)
+print(forecast_df)
