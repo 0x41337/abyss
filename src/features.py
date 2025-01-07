5892577aa545c52f12d973744ea34afe006fb881
tux <041337@proton.me>
2025-01-07 00:09:41 -0300

    Code cleanup and Introduction to Bollinger Bands

--- src/features.py
+++ src/features.py
@@ -1,15 +1,15 @@
 from data import df
 
 # EMA (Exponential Moving Average)
-def f1(span=10):
+def EMA(span=10):
     return df["Close"].ewm(span, adjust=False).mean()
 
 # MACD (Moving Average Convergence Divergence)
-def f2():
-    return f1(12) - f1(26)
+def MACD():
+    return EMA(12) - EMA(26)
 
 # RSI (Relative Strength Index)
-def f3(window=14):
+def RSI(window=14):
     delta = df["Close"].diff(1)
     gain = delta.where(delta > 0, 0)
     loss = -delta.where(delta < 0, 0)
@@ -17,3 +17,11 @@ def f3(window=14):
     avg_loss = loss.rolling(window=window).mean()
     rs = avg_gain / avg_loss
     return 100 - (100 / (1 + rs))
+
+# Bollinger Bands
+def BollingerBands(window=20):
+    rolling_mean = df['Close'].rolling(window=window).mean()
+    rolling_std = df['Close'].rolling(window=window).std()
+    upper_band = rolling_mean + (rolling_std * 2)
+    lower_band = rolling_mean - (rolling_std * 2)
+    return upper_band, lower_band
